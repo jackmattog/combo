f8c5adce89497cff8ab99eb9e068600e16a01f71
jackmattog <jackmattog@gmail.com>
2026-06-29 13:51:36 +0300

    Add urls to main urls.py file

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 from apps.upmatt import upmatt_softwares
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('lesotho/', include('apps.lesotho.urls')),
     path('',include('apps.shared.urls')),
     path('mpc/', include('apps.mpc.urls')),
