7b06b978da12cdb650a124bebbef952b15093034
jackmattog <jackmattog@gmail.com>
2026-06-29 09:00:25 +0300

    Add all apps urls to main config urls.py file

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from apps.upmatt import upmatt_softwares
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('apps.shared.core')),
+    path('lesotho/bar_products/',include('apps.lesotho.bar_products')),
+    path('lesotho/bar_orders/',include('apps.lesotho.bar_orders')),
+    path('lesotho/lesotho_carwash/',include('apps.lesotho.lesotho_carwash')),
+    path('lesotho/lodge_rooms/',include('apps.lesotho.lodge_rooms')),
+    path('mpc/mpc_orders/',include('apps.mpc.mpc_orders')),
+    path('mpc/mpc_products/',include('apps.mpc.mpc_products')),
+    path('accounts/',include('apps.shared.accounts')),
+    path('suggestions/',include('apps.shared.suggestions')),
+    path('upmatt/upmatt_softwares/',include('apps.upmatt.upmatt_softwares')),
+    path('upmatt/web_services/',include('apps.upmatt.web_services')),
 ]
