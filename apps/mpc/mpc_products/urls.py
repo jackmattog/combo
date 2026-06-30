412be63d91a1dd1a4a1641f44ec70eb3fe78a042
jackmattog <jackmattog@gmail.com>
2026-06-30 10:30:21 +0300

    Create urls for mpc products and mpc Product categories using DefautRouter

--- apps/mpc/mpc_products/urls.py
+++ apps/mpc/mpc_products/urls.py
@@ -1,5 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
 
+# 1. Initializing  the DefaultRouter
+router = DefaultRouter()
+
+# 2. Register ViewSets with the router
+router.register(r'categories', views.MpcProductCategoryViewSet, basename='mpc-category')
+router.register(r'products', views.MpcProductViewSet, basename='mpc-product')
+router.register(r'product-images', views.MpcProductImageViewSet, basename='mpc-product-image')
+
+# 3. Including the router URLs in urlpatterns
 urlpatterns = [
-    
+    path('', include(router.urls)),
 ]
